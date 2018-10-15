f06615e9de8ded837db25a12810a3ae4b1fcd0d1
Zynpai <32636952+Zynpai@users.noreply.github.com>
2018-10-15 19:43:40 +0100

    Add files via upload

---
+++ Top down shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    private Rigidbody myRigidbody;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+    private Camera mainCamera;
+
+    public GunController theGun;
+
+    public bool useController;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+
+        //Rotate with Mouse
+        if (!useController)
+        {
+            Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+            Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+            float rayLength;
+
+            if (groundPlane.Raycast(cameraRay, out rayLength))
+            {
+                Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+                Debug.DrawLine(cameraRay.origin, pointToLook, Color.red);
+
+                transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+            }
+
+            if (Input.GetMouseButtonDown(0))
+                theGun.isFiring = true;
+
+            if (Input.GetMouseButtonUp(0))
+                theGun.isFiring = false;
+        }
+
+        //Rotate with Controller
+        if(useController)
+        {
+            Vector3 playerDirection = Vector3.right * Input.GetAxisRaw("RHorizontal") + Vector3.forward * Input.GetAxisRaw("RVertical");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        myRigidbody.velocity = moveVelocity;
+    }
+}
